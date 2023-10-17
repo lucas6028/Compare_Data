2a595875cf8af4d1e51cff815d46651895e3308c
Hao <144909142+lucas6028@users.noreply.github.com>
2023-10-18 07:05:21 +0800

    Upload file

---
+++ compare.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const Excel = require("exceljs");
+
+// Define the directory path and Excel file name
+const directoryPath = "C:/Users/Hao/OneDrive/Documents/data/";
+const fullPath = "C:/Users/Hao/OneDrive/Documents/data/guid.xlsx";
+
+// Read Excel Data
+const workbook = new Excel.Workbook();
+workbook.xlsx
+  .readFile(fullPath)
+  .then(() => {
+    const worksheet = workbook.getWorksheet(1);
+    const excelData = [];
+
+    worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
+      excelData.push(row.values);
+    });
+
+    // Read the directory contents
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        console.error("Error reading directory:", err);
+        return;
+      }
+
+      // Filter out JSON files
+      const jsonFiles = files.filter((file) => file.endsWith(".json"));
+
+      // Process each JSON file
+      jsonFiles.forEach((jsonFile) => {
+        const jsonFilePath = `${directoryPath}${jsonFile}`;
+
+        fs.readFile(jsonFilePath, "utf8", (err, jsonData) => {
+          if (err) {
+            console.error(`Error reading file ${jsonFilePath}:`, err);
+            return;
+          }
+
+          // Process JSON data
+          const parsedJsonData = JSON.parse(jsonData);
+
+          // Compare Data
+          const jsonLine1 = parsedJsonData[0]; // Assuming line1 is an array in the JSON
+
+          for (let i = 0; i < excelData.length; i++) {
+            const excelA = excelData[i][1]; // Assuming excel data is in column A
+            if (jsonLine1 === excelA) {
+              // Write JSON file name to Excel B
+              worksheet.getCell(`B${i + 1}`).value = jsonFile;
+              break; // No need to continue checking
+            }
+          }
+
+          // Save Excel file
+          workbook.xlsx
+            .writeFile("output.xlsx")
+            .then(() => {
+              console.log("Excel file saved with updated data.");
+            })
+            .catch((error) => {
+              console.error(`Error writing Excel file:`, error);
+            });
+        });
+      });
+    });
+  })
+  .catch((error) => {
+    console.error(`Error reading Excel file:`, error);
+  });
