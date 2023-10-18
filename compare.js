8ea8e4488d60663262316abe6a628ccc5bb1b779
Hao <144909142+lucas6028@users.noreply.github.com>
2023-10-18 08:17:32 +0800

    Modifying continue searching

--- compare.js
+++ compare.js
@@ -1,14 +1,13 @@
-const fs = require("fs");
-const Excel = require("exceljs");
+const fs = require('fs');
+const Excel = require('exceljs');
 
 // Define the directory path and Excel file name
-const directoryPath = "C:/Users/Hao/OneDrive/Documents/data/";
-const excelPath = "C:/Users/Hao/OneDrive/Documents/data/guid.xlsx";
+const directoryPath = 'C:/Users/Hao/OneDrive/Documents/data/';
+const excelFileName = 'C:/Users/Hao/OneDrive/Documents/data/guid.xlsx';
 
 // Read Excel Data
 const workbook = new Excel.Workbook();
-workbook.xlsx
-  .readFile(excelPath)
+workbook.xlsx.readFile(excelFileName)
   .then(() => {
     const worksheet = workbook.getWorksheet(1);
     const excelData = [];
@@ -20,18 +19,18 @@ workbook.xlsx
     // Read the directory contents
     fs.readdir(directoryPath, (err, files) => {
       if (err) {
-        console.error("Error reading directory:", err);
+        console.error('Error reading directory:', err);
         return;
       }
 
       // Filter out JSON files
-      const jsonFiles = files.filter((file) => file.endsWith(".json"));
+      const jsonFiles = files.filter(file => file.endsWith('.json'));
 
       // Process each JSON file
-      jsonFiles.forEach((jsonFile) => {
+      jsonFiles.forEach(jsonFile => {
         const jsonFilePath = `${directoryPath}${jsonFile}`;
-
-        fs.readFile(jsonFilePath, "utf8", (err, jsonData) => {
+        
+        fs.readFile(jsonFilePath, 'utf8', (err, jsonData) => {
           if (err) {
             console.error(`Error reading file ${jsonFilePath}:`, err);
             return;
@@ -40,31 +39,37 @@ workbook.xlsx
           // Process JSON data
           const parsedJsonData = JSON.parse(jsonData);
 
-          // Compare Data
-          const jsonLine1 = parsedJsonData[0]; // Assuming line1 is an array in the JSON
+          // Initialize flag for match found
+          let matchFound = false;
 
           for (let i = 0; i < excelData.length; i++) {
             const excelA = excelData[i][1]; // Assuming excel data is in column A
-            if (jsonLine1 === excelA) {
+            if (parsedJsonData.includes(excelA)) {
               // Write JSON file name to Excel B
               worksheet.getCell(`B${i + 1}`).value = jsonFile;
-              break; // No need to continue checking
+              matchFound = true;
+              break; // Match found, no need to continue checking
             }
           }
 
+          // If no match found, consider other comparison logic here
+          if (!matchFound) {
+            // Implement logic to compare other cells or handle no match scenario
+            console.log(`No match found for ${jsonFile}`);
+          }
+
           // Save Excel file
-          workbook.xlsx
-            .writeFile("output.xlsx")
+          workbook.xlsx.writeFile('output.xlsx')
             .then(() => {
-              console.log("Excel file saved with updated data.");
+              console.log('Excel file saved with updated data.');
             })
-            .catch((error) => {
+            .catch(error => {
               console.error(`Error writing Excel file:`, error);
             });
         });
       });
     });
   })
-  .catch((error) => {
+  .catch(error => {
     console.error(`Error reading Excel file:`, error);
   });
